23ff5fa6f8f7f7759467f63b176cab532a6914d4
John6090212 <johnhuang30834@gmail.com>
2020-04-15 17:35:46 +0800

    Update PriorityQueueTest.java

--- Lab2/src/PriorityQueueTest.java
+++ Lab2/src/PriorityQueueTest.java
@@ -30,7 +30,7 @@ public class PriorityQueueTest {
     }
     @Parameters
     public static Collection testNumbers(){
-        return Arrays.asList(new Integer[][] {{2,1,3,1,2,3},{3,2,4,2,3,4},{4,3,5,3,4,5},{5,4,6,4,5,6},{6,5,7,5,6,7}});
+        return Arrays.asList(new Integer[][] {{2,1,3,1,2,4},{3,2,4,2,3,4},{4,3,5,3,4,5},{5,4,6,4,5,6},{6,5,7,5,6,7}});
     }
     /*
     @org.junit.jupiter.api.BeforeEach
